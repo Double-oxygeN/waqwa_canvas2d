29e21e95922862e17e2ba99919e6abb44e73f22b
Double_oxygeN <mail@double-oxygen.net>
2020-03-25 17:13:58 +0900

    :heavy_plus_sign: nothing but component

--- src/waqwa_canvas2d/canvas2dcomp.nim
+++ src/waqwa_canvas2d/canvas2dcomp.nim
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import waqwa_canvas2d/canvas2dcomp
+import scenefw
 
-export Canvas2dComp
+type
+  Canvas2dComp* = ref object of Component
